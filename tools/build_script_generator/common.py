8f9e167e190ce86cd66c443eaa8b164f03500c35
Jens Böckmann <jensboe@outlook.de>
2023-11-10 22:24:50 +0100

    Fix debugger can't find source file.
    Disable file-prefix-map project_source_dir

--- tools/build_script_generator/common.py
+++ tools/build_script_generator/common.py
@@ -229,7 +229,7 @@ def common_compiler_flags(compiler, target):
         "-funsigned-char",
         "-fwrapv",
         # "-fmerge-all-constants",
-        "-ffile-prefix-map={project_source_dir}=.",
+        # "-ffile-prefix-map={project_source_dir}=.",
         "-ffile-prefix-map={gccpath}=.",
 
         "-g3",
